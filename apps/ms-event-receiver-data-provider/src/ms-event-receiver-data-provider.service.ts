d49af38157148483834590d5234dfe076c2bca2d
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 10:05:02 +0300

    feat:(apps/ms-publisher/src) create method to consume messages that published on exchange "exchange1"

--- apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
+++ apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class MsEventReceiverDataProviderService {
-  getHello(): string {
-    return 'Hello World!';
+  private logger: Logger;
+  constructor() {
+    this.logger = new Logger(MsEventReceiverDataProviderService.name);
+  }
+  @RabbitSubscribe({
+    exchange: 'exchange1',
+    routingKey: 'routing.key',
+  })
+  public async consumeMessage(msg: any) {
+    this.logger.log(`Received message: ${JSON.stringify(msg)}`);
   }
 }
