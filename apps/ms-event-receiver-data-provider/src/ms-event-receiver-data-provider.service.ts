3f76ea0db8f016daddada5f9e2b9a0dfd49b1bdf
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 22:35:06 +0300

    feat:(apps/ms-event-receive-data-provider/src) Exposes an RPC through RabbitMQ to fetch last 10 events

--- apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
+++ apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
@@ -1,4 +1,4 @@
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { RabbitSubscribe, RabbitRPC } from '@golevelup/nestjs-rabbitmq';
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { IPublishMessage } from 'libs/common/interfaces/publish-message.interface';
@@ -22,5 +22,18 @@ export class MsEventReceiverDataProviderService {
       },
     });
     this.logger.log(`Received message: ${JSON.stringify(msg)}`);
+  }
+  @RabbitRPC({
+    exchange: 'exchange1',
+    routingKey: 'rpc',
+  })
+  async fetchLast10Events() {
+    const events = await this.prisma.event.findMany({
+      orderBy: { createdAt: 'desc' },
+      take: 10,
+    });
+    this.logger.debug(`new RPC request`);
+
+    return events;
   }
 }
