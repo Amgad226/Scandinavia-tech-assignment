04a20977df706d30aa0a9498a7772927f0d1d91f
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 19:06:45 +0300

    feat:(apps/ms-data-provider) store the message in mongo on consume the rabbit mq message

--- apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
+++ apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.service.ts
@@ -1,10 +1,11 @@
 import { RabbitSubscribe } from 'scandinaviatech-test/node_modules/@golevelup/nestjs-rabbitmq';
 import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class MsEventReceiverDataProviderService {
   private logger: Logger;
-  constructor() {
+  constructor(private readonly prisma: PrismaService) {
     this.logger = new Logger(MsEventReceiverDataProviderService.name);
   }
   @RabbitSubscribe({
@@ -12,6 +13,13 @@ export class MsEventReceiverDataProviderService {
     routingKey: 'routing.key',
   })
   public async consumeMessage(msg: any) {
+    await this.prisma.event.create({
+      data: {
+        description: msg.description,
+        name: msg.name,
+        counter: msg.counter,
+      },
+    });
     this.logger.log(`Received message: ${JSON.stringify(msg)}`);
   }
 }
