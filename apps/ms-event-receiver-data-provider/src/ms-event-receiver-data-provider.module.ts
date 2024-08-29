25b23da01c113d2ff6e6cc591b9d4068ea40ba21
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 19:04:55 +0300

    feat:() init prisma with mongo db

--- apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.module.ts
+++ apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.module.ts
@@ -1,11 +1,15 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
+import { rabbitmqConfig } from 'libs/common/rabbitmq.config';
 import { MsEventReceiverDataProviderController } from './ms-event-receiver-data-provider.controller';
 import { MsEventReceiverDataProviderService } from './ms-event-receiver-data-provider.service';
-import { RabbitMQModule } from 'scandinaviatech-test/node_modules/@golevelup/nestjs-rabbitmq';
-import { rabbitmqConfig } from 'libs/common/rabbitmq.config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [RabbitMQModule.forRoot(RabbitMQModule, rabbitmqConfig)],
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, rabbitmqConfig),
+    PrismaModule,
+  ],
   controllers: [MsEventReceiverDataProviderController],
   providers: [MsEventReceiverDataProviderService],
 })
