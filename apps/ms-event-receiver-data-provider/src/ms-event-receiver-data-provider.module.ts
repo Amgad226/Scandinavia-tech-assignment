0ca6e4498d577acc9bce2676536526db56d83df3
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 09:36:24 +0300

    feat:(apps/publisher/src)configure rabbit mq

--- apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.module.ts
+++ apps/ms-event-receiver-data-provider/src/ms-event-receiver-data-provider.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MsEventReceiverDataProviderController } from './ms-event-receiver-data-provider.controller';
 import { MsEventReceiverDataProviderService } from './ms-event-receiver-data-provider.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { rabbitmqConfig } from 'libs/common/rabbitmq.config';
 
 @Module({
-  imports: [],
+  imports: [RabbitMQModule.forRoot(RabbitMQModule, rabbitmqConfig)],
   controllers: [MsEventReceiverDataProviderController],
   providers: [MsEventReceiverDataProviderService],
 })
