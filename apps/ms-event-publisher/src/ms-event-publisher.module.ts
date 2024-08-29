84c955ac5df68d5816502b397c6789e43dfbc87d
Amgad226 <amgad.wr.1@gmail.com>
2024-08-29 09:35:20 +0300

    feat:(apps/publisher/src)configure rabbit mq

--- apps/ms-event-publisher/src/ms-event-publisher.module.ts
+++ apps/ms-event-publisher/src/ms-event-publisher.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MsEventPublisherController } from './ms-event-publisher.controller';
 import { MsEventPublisherService } from './ms-event-publisher.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { rabbitmqConfig } from 'libs/common/rabbitmq.config';
 
 @Module({
-  imports: [],
+  imports: [RabbitMQModule.forRoot(RabbitMQModule, rabbitmqConfig)],
   controllers: [MsEventPublisherController],
   providers: [MsEventPublisherService],
 })
